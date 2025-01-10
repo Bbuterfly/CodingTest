69166d62a4c391613bad033aa582b9ff62102745
김기수_T7105 <97316837+Bbuterfly@users.noreply.github.com>
2025-01-10 14:45:25 +0900

    [Bronze II] Title: 블랙잭, Time: 36 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,15 +1,24 @@
+from itertools import combinations
+import bisect
 import sys
 
 n, m = map(int, input().split())
 lst = list(map(int, sys.stdin.readline().rstrip().split()))
+lst.sort()
 
 tot_max = 0
-for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            cards = lst[i] + lst[j] + lst[k]
-            
-            if cards <= m and cards > tot_max:
-                tot_max = cards
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        two_sum = lst[i] + lst[j]
+        
+        if two_sum >= m:
+            continue
+        
+        target = m - two_sum
+        idx = bisect.bisect_right(lst, target, j + 1, n) - 1
+        
+        if idx > j:
+            tot_sum = two_sum + lst[idx]
+            tot_max = max(tot_max, tot_sum)
                 
 print(tot_max)
