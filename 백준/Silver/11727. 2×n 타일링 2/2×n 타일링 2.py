e3c387536518dc4921d6ed01743f3359637a1db0
Bbuterfly <97316837+Bbuterfly@users.noreply.github.com>
2025-04-21 18:20:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,18 +1,19 @@
-def tiling():
-    mem = [0] * 1001
-    mem[1], mem[2], mem[3] = 1, 3, 5
-    for i in range(4, 1001):
-        if i%2:
-            mem[i] = mem[i-1] * 2 - 1
-        else:
-            mem[i] = mem[i-1] * 2 + 1
-    
-    return mem
+def tiling(n):
+    mem = [0] * (n + 1)
+    mem[1] = 1
+    if n >= 2:
+        mem[2] = 3
+        
+    for i in range(3, n+1):
+        mem[i] = mem[i-1] + 2 * mem[i-2]
+        mem[i] %= 10007
+        
+    return mem[n]
 
 # n : 2xn 타일의 길이, 1<=n<=10^3
 def main():
     n = int(input())
-    print(tiling()[n]%10007)
+    print(tiling(n))
 
 if __name__ == "__main__":
     main()
